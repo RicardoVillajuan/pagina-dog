cfb7ed9768b4ddc43e8337ad01420057ab5c6f39
Villajuan <solovillajuan@gmail.com>
2021-02-01 15:42:23 -0500

    mi primer commit de la pagina

---
+++ js/menuDesplegable.js
@@ -0,0 +1,5 @@
+let btnMenu=document.getElementById("btn-bars");
+let menuNavegacion=document.getElementById("menu-principal");
+btnMenu.addEventListener("click",function(){
+    menuNavegacion.classList.toggle("mostrar")
+});
