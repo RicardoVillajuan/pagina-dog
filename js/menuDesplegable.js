0aa51e1cc36da86c4358b49c6f6503a5d878f626
RicardoVillajuan <ricardovillajuan23@gmail.com>
2021-02-03 16:20:14 -0500

    Actualizando

--- js/menuDesplegable.js
+++ js/menuDesplegable.js
@@ -3,3 +3,14 @@ let menuNavegacion=document.getElementById("menu-principal");
 btnMenu.addEventListener("click",function(){
     menuNavegacion.classList.toggle("mostrar")
 });
+let ubicacionPrincipal=window.pageYOffset;
+window.onscroll=function(){
+    let desplazamientoActual=window.pageYOffset;
+    if(ubicacionPrincipal>=desplazamientoActual){
+        document.getElementById('banner__logo-principal').style.top='0';
+    }else{
+        document.getElementById('banner__logo-principal').style.top='-100px';
+    }
+    ubicacionPrincipal=desplazamientoActual;
+}
+
