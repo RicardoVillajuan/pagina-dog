efa2081edea90dafc35a4b3089dde653deb3bee4
RicardoVillajuan <ricardovillajuan23@gmail.com>
2021-02-03 22:28:52 -0500

    Version no tan segura

--- js/menuDesplegable.js
+++ js/menuDesplegable.js
@@ -3,26 +3,37 @@ let menuNavegacion=document.getElementById("menu-principal");
 btnMenu.addEventListener("click",function(){
     menuNavegacion.classList.toggle("mostrar")
 });
-
-let noneBg=document.getElementById("banner__logo-none");
 let ubicacionPrincipal=window.pageYOffset;
-// let banner=document.getElementById("banner__logo-principal");
+let banner=document.getElementById("banner__logo-inportante");
 window.onscroll=function(){
     let desplazamientoActual=window.pageYOffset;
-    if(ubicacionPrincipal>=desplazamientoActual){
-        document.getElementById('banner__logo-principal').style.top='0';
-        // if(banner.offsetTop=0){
-        //     banner.classList(".banner__logo-none");
-        // }
-        if(desplazamientoActual==0){
-            document.getElementById('banner__logo-principal').style.background='none';
-            document.getElementById('banner__logo-principal').style.transition='all 0.8s';
+    if(screen.width<=1000){
+        if(ubicacionPrincipal>=desplazamientoActual){
+            document.getElementById('banner__logo-principal').style.top='0';
+            if(desplazamientoActual==0){
+                document.getElementById('banner__logo-principal').style.background='none';
+                document.getElementById('banner__logo-principal').style.transition='all 0.8s';
+            }
+        }else{
+            document.getElementById('banner__logo-principal').style.top='-100px';
+            document.getElementById('banner__logo-principal').style.background='#000000be';
+            
         }
+        ubicacionPrincipal=desplazamientoActual;
     }else{
-        document.getElementById('banner__logo-principal').style.top='-100px';
-        document.getElementById('banner__logo-principal').style.background='#000000be';
-        
+        if(ubicacionPrincipal>=desplazamientoActual){
+            document.getElementById('banner__logo-inportante').style.top='0';
+            if(desplazamientoActual==0){
+                document.getElementById('banner__logo-inportante').style.background='none';
+                document.getElementById('banner__logo-inportante').style.transition='all 0.8s';
+            }
+        }else{
+            document.getElementById('banner__logo-inportante').style.top='-100px';
+            document.getElementById('banner__logo-inportante').style.background='#000000be';
+            
+        }
+        ubicacionPrincipal=desplazamientoActual;
     }
-    ubicacionPrincipal=desplazamientoActual;
+
 }
 
